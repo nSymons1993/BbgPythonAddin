c48d283654dbaafe0e4baea92bcb0d18b5e37b24
Nick Symons <nick.symons@ampcapital.com>
2020-02-16 20:33:33 +1100

    editted setup configuration file

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from distutils.core import setup
 setup(
-  name = 'BloombergData',         # How you named your package folder (MyLib)
-  packages = ['BloombergData'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'A package to enable retrieval of data through the Bloomberg API into pandas dataframes.',   # Give a short description about your library
-  author = 'Nicholas Symons',                   # Type in your name
-  author_email = 'n.symons1993@outlook.com',      # Type in your E-Mail
-  url = 'https://github.com/nSymons1993/AladdinScripts',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/nSymons1993/BbgPythonAddin/archive/v_01.tar.gz',    # I explain this later on
+  name = 'BloombergData',
+  packages = ['BloombergData'],
+  version = '0.1',
+  license='MIT',
+  description = 'A package to enable retrieval of data through the Bloomberg API into pandas dataframes.',
+  author = 'Nicholas Symons',
+  author_email = 'n.symons1993@outlook.com',
+  url = 'https://github.com/nSymons1993/BbgPythonAddin',
+  download_url = 'https://github.com/nSymons1993/BbgPythonAddin/archive/v_01.tar.gz',
   keywords = ['Bloomberg', 'Finance', 'Equities', 'Fixed Income', 'Derivatives', 'Benchmark', 'Index', 'Algorithm', 'Portfolio', 'Trading', 'Management', 'API', 'Financial', 'Machine Learning', 'Statistics', 'FX', 'Options', 'Swaps', 'Swaptions', 'Trade', 'pandas', 'numpy', 'big data'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  install_requires=[
             'blpapi',
             'logging',
             'pandas>=0.24.0',
@@ -20,11 +20,11 @@ setup(
             'tzlocal'
         ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3.6',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7'
   ],
 )
