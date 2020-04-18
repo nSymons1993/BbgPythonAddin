23d455b5c0a04f945823faede7209ba4dca32e93
Nick Symons <nick.symons@ampcapital.com>
2020-04-18 14:15:00 +1000

    updating config, removing logging pip install

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'BloombergData',
   packages = ['BloombergData'],
-  version = '0.2',
+  version = '0.3',
   license='MIT',
   description = 'A package to enable retrieval of data through the Bloomberg API into pandas dataframes.',
   author = 'Nicholas Symons',
   author_email = 'n.symons1993@outlook.com',
   url = 'https://github.com/nSymons1993/BbgPythonAddin',
-  download_url = 'https://github.com/nSymons1993/BbgPythonAddin/archive/v_02.tar.gz',
+  download_url = 'https://github.com/nSymons1993/BbgPythonAddin/archive/v_03.tar.gz',
   keywords = ['Bloomberg', 'Finance', 'Equities', 'Fixed Income', 'Derivatives', 'Benchmark', 'Index', 'Algorithm', 'Portfolio', 'Trading', 'Management', 'API', 'Financial', 'Machine Learning', 'Statistics', 'FX', 'Options', 'Swaps', 'Swaptions', 'Trade', 'pandas', 'numpy', 'big data'],
   install_requires=[
             'blpapi',
